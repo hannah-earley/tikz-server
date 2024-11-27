aa4f3535b7458126dec512ddbd685ec78ac0e0e7
Hannah Earley <h@nnah.io>
2024-11-26 23:25:24 -0600

    add multithreading support

--- compile.py
+++ compile.py
@@ -17,7 +17,24 @@ def svg_uri(svg):
 def binary_uri(image, mimetype):
   return f"data:{mimetype};base64,{base64.b64encode(image).decode()}"
 
-def process_script(script, fmt, preamble, source):
+def preprocess_script(fmt, preamble, source, verbose=False):
+  """
+  Prerender scripts; intended to be used with multithreading to accelerate compilation.
+  """
+
+  try:
+    format_ = config.formats[fmt]
+    kw = {**format_['options'], 'preamble': preamble}
+    filename = cache.filename(preamble, source, fmt)
+    image = cache.cached(format_['renderer'])(filename, source, **kw)
+
+    if verbose:
+      print('.', end='', file=sys.stderr, flush=True)
+
+  except Exception as e:
+    pass
+
+def process_script(script, fmt, preamble, source, verbose=False):
   """
   Replace the given script element with a rendered copy. This behavior should
   be identical to that of tikz.js (at least when no errors occur).
@@ -54,10 +71,13 @@ def process_script(script, fmt, preamble, source):
     element['style'] = f"width:{w_em}em;"
     script.replace_with(element)
 
+    if verbose:
+      print('.', end='', file=sys.stderr, flush=True)
+
   except Exception as e:
     print(e, file=sys.stderr)
 
-def process(file, fmt, inplace=False, preserve_ws=True):
+def process(file, fmt, inplace=False, preserve_ws=True, threading=False):
   with open(file, 'r') as fp:
     contents = fp.read()
 
@@ -75,13 +95,25 @@ def process(file, fmt, inplace=False, preserve_ws=True):
     preamble += script.string
     script.decompose()
 
+  if threading:
+    import concurrent.futures
+    with concurrent.futures.ThreadPoolExecutor(max_workers=threading) as executor:
+      for script in soup.find_all("script", type="tex"):
+        content = script.string
+        executor.submit(preprocess_script, fmt, preamble, content, verbose=True)
+      for script in soup.find_all("script", type="tikz"):
+        content = "\\begin{tikzpicture}" + script.string + "\\end{tikzpicture}"
+        executor.submit(preprocess_script, fmt, preamble, content, verbose=True)
+
   for script in soup.find_all("script", type="tex"):
     content = script.string
-    process_script(script, fmt, preamble, content)
+    process_script(script, fmt, preamble, content, verbose=not threading)
 
   for script in soup.find_all("script", type="tikz"):
     content = "\\begin{tikzpicture}" + script.string + "\\end{tikzpicture}"
-    process_script(script, fmt, preamble, content)
+    process_script(script, fmt, preamble, content, verbose=not threading)
+
+  print('', file=sys.stderr)
 
   if preserve_ws:
     output = str(soup.pre)
@@ -97,6 +129,8 @@ def process(file, fmt, inplace=False, preserve_ws=True):
     print(output)
 
 def main():
+  cache.clean()
+
   parser = argparse.ArgumentParser(add_help=False, description="Replaces tikz/tex 'scripts' in an HTML file(s) with rendered copies of specified format.")
   parser.add_argument('-h', '-help', help="Show this help message", action='help')
 
@@ -108,11 +142,18 @@ def main():
 
   parser.add_argument('-inplace', action='store_true', help="Overwrite input files in-place, otherwise outputs to stdout")
   parser.add_argument('-preserve', action='store_true', help="Preserve whitespace")
+  parser.add_argument('-threading', action='store', type=int, metavar='T', help="Activate threading with T threads (T=0 uses number of CPU cores)", default=False)
   parser.add_argument('files', metavar='file', type=str, nargs='+', help="Input HTML file(s)")
 
   args = parser.parse_args()
+
+  cores = args.threading
+  if cores == 0:
+    import multiprocessing
+    cores = multiprocessing.cpu_count()
+
   for file in args.files:
-    process(file, fmt=args.format, inplace=args.inplace, preserve_ws=args.preserve)
+    process(file, fmt=args.format, inplace=args.inplace, preserve_ws=args.preserve, threading=cores)
 
 if __name__ == '__main__':
   main()
